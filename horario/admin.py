e9ce2a718fbf4f105442cfa897ffb7aae5f67a2e
roviol <rolandoviol@gmail.com>
2015-10-13 06:06:36 -0400

    'estructura'

--- horario/admin.py
+++ horario/admin.py
@@ -2,6 +2,38 @@ from django.contrib import admin
 
 # Register your models here.
 
+from horario.models import Pais
+from horario.models import Ciudad
 from horario.models import Colegio
+from horario.models import Curso
+from horario.models import Seccion
+from horario.models import Materia
+from horario.models import MateriaCurso
+from horario.models import Horario
+from horario.models import HorarioDia
+from horario.models import HorarioDetalle
 
-admin.site.register(Colegio)
+class ColegioInline(admin.TabularInline):
+    model = Colegio
+    extra = 1
+
+class CiudadAdmin(admin.ModelAdmin):
+    inlines = [ColegioInline]
+
+
+class HorarioDiaInline(admin.TabularInline):
+    model = HorarioDia
+    extra = 1
+
+class HorarioAdmin(admin.ModelAdmin):
+    inlines = [HorarioDiaInline]
+
+admin.site.register(Pais)
+admin.site.register(Ciudad, CiudadAdmin)
+admin.site.register(Curso)
+admin.site.register(Seccion)
+admin.site.register(Materia)
+admin.site.register(MateriaCurso)
+admin.site.register(Horario,HorarioAdmin)
+admin.site.register(HorarioDia)
+admin.site.register(HorarioDetalle)
--- horario/models.py
+++ horario/models.py
@@ -1,8 +1,91 @@
 from django.db import models
+import myFields
 
-# Create your models here.
-class Colegio(models.Model):
+DAYS_OF_WEEK = (
+    (0, 'Domingo'),
+    (1, 'Lunes'),
+    (2, 'Martes'),
+    (3, 'Miercoles'),
+    (4, 'Jueves'),
+    (5, 'Viernes'),
+    (6, 'Sabado'),
+)
+
+class Pais(models.Model):
     nombre = models.CharField(max_length=100)
     def __unicode__(self):
         return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+        verbose_name_plural = 'Paises'
 
+class Ciudad(models.Model):
+    pais = models.ForeignKey(Pais)
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+        verbose_name_plural = 'Ciudades'
+
+class Colegio(models.Model):
+    ciudad = models.ForeignKey(Ciudad)
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+
+class Curso(models.Model):
+    pais = models.ForeignKey(Pais)
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+
+class Seccion(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+        verbose_name_plural = 'Secciones'
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"%s" % (self.nombre)
+    class Meta:
+        ordering = ['nombre']
+
+class MateriaCurso(models.Model):
+    curso = models.ForeignKey(Curso)
+    materia = models.ForeignKey(Materia)
+    def __unicode__(self):
+        return u"%s %s" % (self.curso.nombre, self.materia.nombre)
+
+class Horario(models.Model):
+    colegio = models.ForeignKey(Colegio)
+    curso = models.ForeignKey(Curso)
+    seccion = models.ForeignKey(Seccion)
+    desde = models.DateField()
+    hasta = models.DateField()
+    def __unicode__(self):
+        return u"%s %s %s" % (self.colegio.nombre, self.curso.nombre, self.seccion.nombre)
+    class Meta:
+        ordering = ['-desde']
+
+class HorarioDia(models.Model):
+    horario = models.ForeignKey(Horario)
+    dia = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    def __unicode__(self):
+        return u"%s %s" % (self.horario.curso.nombre, self.dia)
+    class Meta:
+        ordering = ['dia']
+
+class HorarioDetalle(models.Model):
+    horariodia = models.ForeignKey(HorarioDia)
+    desde = models.TimeField()
+    hasta = models.TimeField()
+    materia = models.ForeignKey(Materia)
